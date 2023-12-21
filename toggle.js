1601f9df1e88ad8f9358d6385b882a068bba67ea
maxwett <95075807+maxwett@users.noreply.github.com>
2023-12-21 17:14:38 +0100

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,22 +1,7 @@
 function loadPlot() {
     var plotSelector = document.getElementById('plotSelector');
-    var plotContainer = document.getElementById('plot-container');
+    var plotFrame = document.getElementById('plotFrame');
 
-    // Get the selected option's value (HTML file path)
     var selectedPlot = plotSelector.value;
-
-    // Load and display the HTML content
-    fetch(selectedPlot)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
-            }
-            return response.text();
-        })
-        .then(html => {
-            plotContainer.innerHTML = html;
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    plotFrame.src = selectedPlot;
 }
